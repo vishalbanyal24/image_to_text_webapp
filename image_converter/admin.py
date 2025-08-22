146f475610cff8f324335d5d3df1f0ba6027d88d
Vishal Banyal <121677457+vishalbanyal24@users.noreply.github.com>
2025-08-22 16:00:16 +0530

    Delete image_converter directory

--- image_converter/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ImageDescription
-
-# Register your models here.
-admin.site.register(ImageDescription)
--- image_converter/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.convert_image_to_text, name='convert_image_to_text'),
-]
