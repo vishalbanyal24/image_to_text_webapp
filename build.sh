0d32af36e3bfd6eacaeb0bf979fc62e0c9638ca7
Vishal Banyal <121677457+vishalbanyal24@users.noreply.github.com>
2025-08-26 17:45:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Current value of GOOGLE_API_KEY:"
+echo $GOOGLE_API_KEY
+
+
 echo "Build completed successfully!"
