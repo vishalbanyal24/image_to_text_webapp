b31863f4c21598dc4881e24c373a6dec31f31c54
Vishal Banyal <121677457+vishalbanyal24@users.noreply.github.com>
2025-08-27 13:02:40 +0530

    Update setup_env.bat

--- setup_env.bat
+++ setup_env.bat
@@ -2,14 +2,18 @@
 echo Setting up environment for Image to Text Generator...
 echo.
 
+:: Ask for API Key input
 echo Please enter your Google API Key:
 set /p GOOGLE_API_KEY="API Key: "
-echo $GOOGLE_API_KEY
+
+:: Show the entered key
+echo You entered: %GOOGLE_API_KEY%
 
 echo.
 echo Setting environment variable GOOGLE_API_KEY...
 setx GOOGLE_API_KEY "%GOOGLE_API_KEY%" >nul
 
+echo.
 echo Current value of GOOGLE_API_KEY:
 echo %GOOGLE_API_KEY%
 
@@ -24,7 +28,3 @@ echo Note: Using 'setx' makes the variable permanent for future sessions.
 echo You may need to open a new terminal for it to take effect.
 echo.
 pause
-
-
-
-
